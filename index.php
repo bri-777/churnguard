76a23b7cc09e8fb6df6ab87ee4a2620c6662bf13
bri-777 <briandericamador3@gmail.com>
2025-10-08 08:44:04 +0800

    s

--- index.php
+++ index.php
@@ -3156,7 +3156,7 @@ console.log('[APP] Sales Analytics Dashboard Script Loaded')
 .history-section {
   position: absolute;
   top: 650px;
-  left: 64%;
+  left: 61%;
   transform: translateX(-50%);
   width: 80%; /* wide, but still leaves some margin */
 }
