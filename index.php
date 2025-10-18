f85bce3622481184f5a759ddc5654d3014fb75f6
bri-777 <briandericamador3@gmail.com>
2025-10-18 10:04:17 +0800

    s

--- index.php
+++ index.php
@@ -1718,7 +1718,7 @@ html, body {
 
 
 
-.kpi-card revenue{
+.kpi-content{
 
 background: linear-gradient(135deg, var(--color-primary-dark) 0%, var(--color-primary) 50%, var(--color-primary-light) 100%);
 
