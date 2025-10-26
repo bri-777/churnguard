7760da366a8eee9dd27d2f5906412546f5d52302
bri-777 <briandericamador3@gmail.com>
2025-10-26 23:39:44 +0800

    s

--- index.php
+++ index.php
@@ -6412,14 +6412,7 @@ class CustomerInsights {
                         </svg>
                     </div>
                 </div>
-                <div class="profile-actions">
-                    <button class="btn-icon" title="View Details" onclick="window.customerInsights.viewCustomerDetails('${customer.customer_name}')">
-                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/>
-                            <circle cx="12" cy="12" r="3"/>
-                        </svg>
-                    </button>
-                </div>
+                
             </div>
         `).join('');
     }
