e49934e890316f2dd6e677d57b06c80900b3320f
bri-777 <briandericamador3@gmail.com>
2025-10-26 20:10:47 +0800

    d

--- index.php
+++ index.php
@@ -6110,16 +6110,7 @@ body {
         
         <div class="purchase-analytics">
           <div class="purchase-overview">
-            <div class="overview-stat">
-              <div class="stat-icon">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                  <rect x="3" y="3" width="18" height="18" rx="2" ry="2"/>
-                  <line x1="12" y1="8" x2="12" y2="16"/>
-                  <line x1="8" y1="12" x2="16" y2="12"/>
-                </svg>
-              </div>
-            
-            </div>
+          
             
             <div class="overview-stat">
              
